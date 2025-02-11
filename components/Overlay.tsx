96f4a38b313a69d04751fc538e40d94230be6bd1
Tanay Gandhi <tgandhi.dev@gmail.com>
2025-02-11 13:07:11 -0800

    tweaks

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -12,12 +12,19 @@ export default function Overlay({ open }: OverlayProps) {
   }
 
   return (
-    <Dialog open={true} onOpenChange={() => false} >
+    <Dialog open={open} onOpenChange={() => false} >
       <DialogContent className="overlay-content h-screen">
       <ReactConfetti className="" />
         <h2 className="mt-40">You collected all 5 memories!</h2>
         <h3>Will you be my Valentine?</h3>
-        <div className="flex flex-row">
+        <div className="flex flex-row relative">
+        <Image
+          src="/emily.png"
+          className="z-40 absolute top-[-100px] left-1/2 h-[100px] w-[90px] animate-bounce duration-500" 
+          width={60}
+          height={80}
+          alt="Emily"
+         />
         {[...Array(5)].map((_, i) => (
           <Image
             src={`/couplepic${i + 1}.jpeg`}
